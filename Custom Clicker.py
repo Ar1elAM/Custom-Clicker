7c4b51f4d32b5bae64eafd78d8ddda65c9903574
Ariel <arielaug16@gmail.com>
2022-10-11 14:27:00 +0300

    Removed a few unecessary lines And added an "installer" for 2 modules

--- Custom Clicker.py
+++ Custom Clicker.py
@@ -1,9 +1,22 @@
-from pynput.mouse import Button, Controller
-from pynput.keyboard import Listener, KeyCode
-import pyautogui
+import os
 import threading
 import random
 import time
+
+try:
+    from pynput.mouse import Button, Controller
+except ImportError:
+    print("Trying to Install required module: pynput\n")
+    os.system('python -m pip install pynput')
+    from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+
+try:
+    import pyautogui
+except ImportError:
+    print("Trying to Install required module: pyautogui\n")
+    os.system('python -m pip install pyautogui')
+    import pyautogui
 
 pyautogui.FAILSAFE = False
 mouse_pos = [0]
@@ -12,9 +25,12 @@ button = Button.right
 custom = False
 delay = 0.5
 print('After setting up hotkeys, the mouse will move to get screen limits.\nPlease don\'t move the mouse until you read "Done"\n')
-set_XY_key = KeyCode(char=input("Please choose a key to set custom mouse positions:"))
-start_stop_key = KeyCode(char=input("Please choose a key to start/stop mouse movement:"))
+set_XY_key = KeyCode(char=input(
+    "Please choose a key to set custom mouse positions:"))
+start_stop_key = KeyCode(char=input(
+    "Please choose a key to start/stop mouse movement:"))
 quit_key = KeyCode(char=input("Please choose a key to quit the program:"))
+
 
 class AutoMouse(threading.Thread):
 
@@ -66,43 +82,47 @@ class AutoMouse(threading.Thread):
                     pos_x = (r_x)
                     pos_y = (r_y)
                     pyautogui.moveTo(pos_x, pos_y)
-                    time.sleep(r_timer)
+                    time.sleep(timer)
                 else:
                     x = 1
-                    while(x<len(mouse_pos)):
+                    while (x < len(mouse_pos)):
                         pyautogui.click(mouse_pos[x])
                         time.sleep(mouse_time[x])
-                        x+=1
+                        x += 1
                 time.sleep(5)
+
 
 mouse = Controller()
 auto_thread = AutoMouse(delay, button)
 auto_thread.start()
 
+
 def key_press(key):
     # start_stop_key will stop clicking
     global custom
-    #custom_in = custom
     if key == set_XY_key:
         if (custom == True):
             current_mouse_pos = mouse.position
-            mouse_pos.append(current_mouse_pos)
-            mouse_time.append(float(input("\nPlease choose a value(in seconds) for the time you wish to wait between actions:")))
-            #print(mouse_pos)
+            mouse_pos.append(current_mouse_pos)  # type: ignore
+            mouse_time.append(float(input(
+                "\nPlease choose a value(in seconds) for the time you wish to wait between actions:")))  # type: ignore
         else:
             custom = True
-            print("\nYou chose custom mode, this mode is useful for precise clicks positioning.\nJust hover your mouse where you want it to click, and press the ",set_XY_key," key, and type the amount of seconds you want it to wait.\nWhen ready just press the button you chose as " '"start button": ',start_stop_key, " key.")
+            print("\nYou chose custom mode, this mode is useful for precise clicks positioning.\nJust hover your mouse where you want it to click, and press the ", set_XY_key,
+                  " key, and type the amount of seconds you want it to wait.\nWhen ready just press the button you chose as " '"start button": ', start_stop_key, " key.")
     elif key == start_stop_key:
         if auto_thread.running:
             auto_thread.stop_clicking()
             custom = False
         else:
             auto_thread.start_clicking()
-            print("\nAt any moment you can press the ", quit_key, " key to close the script, or the ", start_stop_key, " to stop.\nThe script will keep in loop until one of the previous keys are pressed\nThe program will stop when the set have finished.")
+            print("\nAt any moment you can press the ", quit_key, " key to close the script, or the ", start_stop_key,
+                  " to stop.\nThe script will keep in loop until one of the previous keys are pressed\nThe program will stop when the set have finished.")
     elif key == quit_key:
         listener.stop()
         auto_thread.stop_clicking()
         auto_thread.exit()
+
 
 with Listener(on_press=key_press) as listener:
     listener.join()
